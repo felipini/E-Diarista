8c5e08d07066b1e17de850a1c41d1d8e450c1d01
felipini <felipini@gmail.com>
2022-12-20 15:59:39 -0300

    Finalizando componentes de Link

---
+++ src/UI/components/navigation/Link/Link.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren } from "react";
+import NextLink, { LinkProps as NextLinkProps } from "next/link";
+import { Link as MuiLink, LinkProps as MuiLinkProps, ButtonProps} from "@mui/material";  
+import Router from "next/router"; 
+
+interface LinkProps {
+    href: string;
+    mui?: MuiLinkProps | ButtonProps;
+    next?: NextLinkProps;
+    Component?: React.ElementType;
+}
+
+const Link: React.FC<PropsWithChildren<LinkProps>> = ({ 
+    children,
+    href,
+    next,
+    mui,
+    Component = MuiLink,
+    ...propos
+}) => {
+    const isNextEnv = Boolean(Router.router);
+
+    return isNextEnv ? (
+        <NextLink href={href} passHref {...next}>
+            <Component {...mui} {...propos}>{children}</Component>
+        </NextLink>
+    ) : (
+        <Component href={href} {...mui} {...propos}>{children}</Component>
+    );
+};
+
+export default Link;
